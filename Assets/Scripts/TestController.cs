beb7eeeec561428ca700ffbcb5ab5033b6649807
원자로개구리 <jhjjgu0115@naver.com>
2017-05-13 21:02:19 +0900

    애니메이션 실행 테스트 공간 생성
    배경을 이후 만들것

---
+++ Assets/Scripts/TestController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour {
+
+    public float x = 1;
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, Vector3.zero, Time.deltaTime * x);
+		
+	}
+}
