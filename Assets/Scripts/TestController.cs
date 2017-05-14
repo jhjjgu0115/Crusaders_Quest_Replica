cd34350d572e7e441cf97ab817e2626b2258a6ee
원자로개구리 <jhjjgu0115@naver.com>
2017-05-15 00:23:11 +0900

    아르니또 애니메이션 일부 작업

--- Assets/Scripts/TestController.cs
+++ Assets/Scripts/TestController.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TestController : MonoBehaviour {
-
+    public Vector3 startPosion = new Vector3(0,-1.956f,0);
     public float x = 1;
-	// Update is called once per frame
-	void Update () {
-        transform.position = Vector3.MoveTowards(transform.position, Vector3.zero, Time.deltaTime * x);
+    public Animator animator;
+    bool isEnd = false;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, startPosion, Time.deltaTime * x);
+        if(transform.position.x<1.5005f && !isEnd)
+        {
+            isEnd = true;
+            animator.Play("Victory",0,0.01f);
+            animator.SetBool("inBattle", false);
+        }
 		
 	}
 }
