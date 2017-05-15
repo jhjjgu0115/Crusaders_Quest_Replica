9bf98b41ee31c62148076fc67b6a0aa4db7b0ffa
원자로개구리 <jhjjgu0115@naver.com>
2017-05-15 20:20:54 +0900

    아르니또 애니메이션 완성
    인트로 애니메이션 완성

---
+++ Assets/Scripts/SkinReader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Anima2D;
+
+namespace CrusadersQuest
+{
+    public class SkinReader : MonoBehaviour
+    {
+        [SerializeField]
+        [HideInInspector]
+        List<Pose> m_Poses;
+    }
+}
