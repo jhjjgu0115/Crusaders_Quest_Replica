509748d5e69fa324cd733e68847eb7c617cb3eb9
원자로개구리 <jhjjgu0115@naver.com>
2017-05-18 02:55:46 +0900

    기본 생성자 생성

--- Assets/Scripts/System/Unit.cs
+++ Assets/Scripts/System/Unit.cs
@@ -2,19 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : MonoBehaviour {
+public class Unit : MonoBehaviour
+{
     
     
-
-
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
