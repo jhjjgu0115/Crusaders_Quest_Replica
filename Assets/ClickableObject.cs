0dd874de149073ff914137a4845071f6ccad33d7
원자로개구리 <jhjjgu0115@naver.com>
2017-10-25 13:25:38 +0900

    로비 백그라운드 무빙 완료
    메뉴및 진입 루트 제작 해야됨

---
+++ Assets/ClickableObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableObject : MonoBehaviour {
+    SpriteRenderer spriteRenderer;
+
+    private void OnMouseDown()
+    {
+        spriteRenderer.color = new Color32(150, 150, 150, 255);
+    }
+    private void OnMouseUp()
+    {
+        spriteRenderer.color = new Color32 (255, 255, 255,255);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+    }
+}
---
+++ Assets/ScrollableObject.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollableObject : MonoBehaviour
+{
+    public float minX=-7.7f;
+    public float maxX=2.4f;
+    public float dragSpeed=1;
+    public float velocity;
+    public float smooth;
+
+    public static ScrollableObject coroutineObject;
+    public static Coroutine scrollCoroutine;
+
+
+    private void OnMouseDrag()
+    {
+        float x = Input.GetAxis("Mouse X");
+        velocity = x;
+        CameraController.instance.transform.position += new Vector3(x* dragSpeed, 0, 0);
+    }
+    private void OnMouseDown()
+    {
+        if (scrollCoroutine!=null)
+        {
+            coroutineObject.StopCoroutine(scrollCoroutine);
+        }
+
+    }
+
+    private void OnMouseUp()
+    {
+        if (scrollCoroutine != null)
+        {
+            coroutineObject.StopCoroutine(scrollCoroutine);
+        }
+        coroutineObject = this;
+        scrollCoroutine = StartCoroutine(SmoothMoving());
+    }
+    
+    IEnumerator SmoothMoving()
+    {
+        while(true)
+        {
+            CameraController.instance.transform.position += new Vector3(velocity * dragSpeed, 0, 0);
+            velocity *= smooth;
+            if(velocity>0.01f || velocity < -0.01f)
+            {
+            }
+            else
+            {
+                break;
+            }
+            yield return null;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start ()
+    {
+	}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
