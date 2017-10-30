833936bf80e2417eb5ae8bee61757ad3b44e4f76
원자로개구리 <jhjjgu0115@naver.com>
2017-10-30 20:40:22 +0900

    캐릭터 선택창 토글 기능 및 이것저것 추가.
    정리하는게 더힘든것같다.

---
+++ Assets/HeroButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HeroButton : MonoBehaviour {
+
+    public Image heroImage;
+    public Image backGroundImage;
+    public RectTransform heroImageRect;
+    public Sprite noneSelectButtonImage;
+    public Sprite selectedButtonImage;
+    public Toggle toggle;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        toggle = GetComponent<Toggle>();
+        backGroundImage = GetComponent<Image>();
+        heroImage.SetNativeSize();
+        Vector2 size = heroImageRect.sizeDelta;
+        Vector2 pixelPivot = heroImage.sprite.pivot;
+        Vector2 percentPivot = new Vector2(pixelPivot.x / size.x, pixelPivot.y / size.y);
+        heroImageRect.pivot = percentPivot;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ChangeSprite()
+    {
+        if(toggle.isOn)
+        {
+            backGroundImage.sprite = selectedButtonImage;
+        }
+        else
+        {
+            backGroundImage.sprite = noneSelectButtonImage;
+        }
+    }
+}
